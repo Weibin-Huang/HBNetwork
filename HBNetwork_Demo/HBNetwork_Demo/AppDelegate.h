1ae78624652839f41ddb869eb92b114d12660c22
AlanWong <AlanWong@hebao032.local>
2015-10-27 11:49:09 +0800

    【新增】创建新项目

---
+++ HBNetwork_Demo/HBNetwork_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBNetwork_Demo
+//
+//  Created by AlanWong on 15/10/27.
+//  Copyright © 2015年 AlanWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBNetwork_Demo/HBNetwork_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBNetwork_Demo
+//
+//  Created by AlanWong on 15/10/27.
+//  Copyright © 2015年 AlanWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
