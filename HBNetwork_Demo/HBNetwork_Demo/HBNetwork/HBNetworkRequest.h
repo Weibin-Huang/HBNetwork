94b06066bf59e6e7ce4e8b8b94d8d28dc878d3ea
AlanWong <AlanWong@hebao032.local>
2015-10-28 18:12:57 +0800

    【修改】HBNetworkRequest

--- HBNetwork_Demo/HBNetwork_Demo/HBNetwork/HBNetworkRequest.h
+++ HBNetwork_Demo/HBNetwork_Demo/HBNetwork/HBNetworkRequest.h
@@ -7,7 +7,55 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "HBNetworkResponse.h"
+@class HBNetworkRequest;
+
+typedef HBNetworkRequest * (^HBNetworkRequestBlock)(id key, ...);
+typedef void (^HBNetworkRequestFinishedBlock)( HBNetworkRequest * req );
+typedef void (^HBNetworkRequestUpdateProgressBlock)( HBNetworkRequest * req );
+typedef void (^HBNetworkRequestDownloadProgressBlock)( HBNetworkRequest * req );
+
+typedef NS_ENUM(NSInteger,HBNetworkRequestSerializer){
+    HBNetworkRequestSerializerDefault      = 0,
+    HBNetworkRequestSerializerJSON         = 1,
+    HBNetworkRequestSerializerPropertyList = 2,
+};
+
+typedef NS_ENUM(NSInteger,HBNetworkResponseSerializer){
+    HBNetworkResponseSerializerDefault      = 0,
+    HBNetworkResponseSerializerXML          = 1,
+    HBNetworkResponseSerializerPropertyList = 2,
+    HBNetworkResponseSerializerImageLoader  = 3,
+};
 
 @interface HBNetworkRequest : NSObject
 
+@property (nonatomic, readonly) HBNetworkRequestBlock header;
+@property (nonatomic, readonly) HBNetworkRequestBlock body;
+@property (nonatomic, readonly) HBNetworkRequestBlock param;
+@property (nonatomic, readonly) HBNetworkRequestBlock file;
+
+@property (nonatomic, readonly) long long bytesRead;
+@property (nonatomic, readonly) long long bytesReadTotal;
+@property (nonatomic, readonly) long long bytesWritten;
+@property (nonatomic, readonly) long long bytesWrittenTotal;
+
+@property (nonatomic, readonly) id responseObject;
+@property (nonatomic, strong, readwrite) HBNetworkResponse * response;
+
+@property (nonatomic, copy, readwrite) HBNetworkRequestFinishedBlock         finishedBlcok;
+@property (nonatomic, copy, readwrite) HBNetworkRequestUpdateProgressBlock   updateProgressBlock;
+@property (nonatomic, copy, readwrite) HBNetworkRequestDownloadProgressBlock downloadProgressBlock;
+
+@property (nonatomic, copy) NSString * httpMethod;
+@property (nonatomic, copy) NSString * baseUrl;
+@property (nonatomic, strong) NSURL * requestUrl;
+@property (nonatomic, assign) BOOL encodingParamsInURI;
+@property (nonatomic, assign) HBNetworkRequestSerializer requestSerializer;
+@property (nonatomic, assign) HBNetworkResponseSerializer responseSerializer;
+
++ (HBNetworkRequest *)requestWithRequest:(NSURLRequest *)request;
++ (HBNetworkRequest *)requestWithURLString:(NSString *)URLString;
++ (HBNetworkRequest *)requestWithURLString:(NSString *)URLString
+                             completeBlock:(HBNetworkRequestFinishedBlock)completeBlock;
 @end
--- HBNetwork_Demo/HBNetwork_Demo/HBNetwork/HBNetworkRequest.m
+++ HBNetwork_Demo/HBNetwork_Demo/HBNetwork/HBNetworkRequest.m
@@ -7,6 +7,24 @@
 //
 
 #import "HBNetworkRequest.h"
+#import "AFNetworking.h"
+@interface HBNetworkRequest()
+@property (nonatomic,strong) NSMutableDictionary * headers;
+@property (nonatomic,strong) NSMutableDictionary * files;
+@property (nonatomic,strong) NSData * body;
+@property (nonatomic,strong) NSMutableDictionary * params;
+@property (nonatomic,strong) NSMutableURLRequest * httpRequest;
+@property (nonatomic,strong) NSURL * url;
+@property (nonatomic,strong) AFHTTPRequestOperation * httpRequestOperation;
+@property (nonatomic,strong) AFHTTPRequestSerializer * httpRequestSerializer;
+
+@property (nonatomic,assign) long long bytesRead;
+@property (nonatomic,assign) long long bytesReadTotal;
+
+@property (nonatomic,assign) long long bytesWritten;
+@property (nonatomic,assign) long long bytesWrittenTotal;
+
+@end
 
 @implementation HBNetworkRequest
 
